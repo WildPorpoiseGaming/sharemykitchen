70203565fae04605d78e3a2b2b3ddc1da307e25a
WildPorpoiseGaming <wildporpoisegaming1@gmail.com>
2017-06-21 15:33:35 -0500

    adds home

--- src/shared/component/page/home.jsx
+++ src/shared/component/page/home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import { APP_NAME } from '../../config'
+import Home from '../../container/home'
 
 const HomePage = () => {
   return (
@@ -12,74 +13,7 @@ const HomePage = () => {
           { property: 'og:title', content: APP_NAME },
         ]}
       />
-
-      <form>
-        <h3>Oven Type</h3>
-        <label>
-          <input type="radio" name="stove" value="gas" />
-        Gas Stove
-      </label>
-        <label>
-          <input type="radio" name="stove" value="electric" />
-      Electric Stove
-    </label>
-      <label>
-        <input type="radio" name="oven" value="nopref" />
-    No Preference
-  </label>
-        <h3>Stove Type</h3>
-        <label>
-          <input type="radio" name="oven" value="gas" />
-        Gas Oven
-      </label>
-      <label>
-        <input type="radio" name="oven" value="electric" />
-      Electric Oven
-    </label>
-      <label>
-        <input type="radio" name="oven" value="nopref" />
-    No Preference
-  </label>
-        <h3>Mixer Type</h3>
-        <label>
-          <input type="radio" name="mixer" value="kitchenaid" />
-        Kitchen Aid Mixer
-      </label>
-        <label>
-          <input type="radio" name="mixer" value="other" />
-      Other
-    </label>
-      <label>
-        <input type="radio" name="mixer" value="nopref" />
-    No Preference
-  </label>
-        <h3>Blender Type</h3>
-        <label>
-          <input type="radio" name="blender" value="vitamix" />
-        Vitamix
-      </label>
-        <label>
-          <input type="radio" name="blender" value="other" />
-      Other
-    </label>
-        <label>
-          <input type="radio" name="blender" value="nopref" />
-    No Preference
-  </label>
-        <h3>Refrigerator Type</h3>
-        <label>
-          <input type="radio" name="refrigerator" value="stainless" />
-        Stainless Steel
-      </label>
-        <label>
-          <input type="radio" name="refrigerator" value="deepfreeze" />
-      Deep Freeze
-    </label>
-      <label>
-        <input type="radio" name="refrigerator" value="nopref" />
-    No Preference
-  </label>
-      </form>
+      <Home />
     </div>
   )
 }
